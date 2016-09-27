ef23f581471982a66b6a8e595f361e866e9783ad
jbousquie <jerome@bousquie.fr>
2016-09-27 10:59:01 +0200

    ajout anchors

--- admin/reload_salles.php
+++ admin/reload_salles.php
@@ -24,64 +24,64 @@ Function Get_salles_bloquees() {
     return $salles_bloquees;
 }
 
-echo "<b>Connexions Windows en cours par salle</b><br/><br/>";
-echo "<br/>";
+
 // connexions dans les salles
 //$salles_bloquees = Get_salles_bloquees();
 
 while ($mdc = current($machines_de_salle)) {
     $salle = key($machines_de_salle);
-    if (!in_array($salle, $salles_invisibles) ) {
-      $bloque = '<i><a href="bloque_salle.php?a=b&s='.strtolower($salle).'">bloque</a></i>';
-      $debloque = '<i><a href="bloque_salle.php?a=d&s='.strtolower($salle).'">debloque</a></i>';
-      $lien = $bloque;
-      // calcul dernière plus ancienne connexion
-      $jours_last_con = Connexion_doyenne_salle($machines_de_salle[$salle]);
-      $class_jour ='jours ';
-      if ($jours_last_con > 9)  { $class_jour = 'jours j10'; }
-      if ($jours_last_con > 19) { $class_jour = 'jours j20'; }
-      if ($jours_last_con > 29) { $class_jour = 'jours j30'; }
+    if (!in_array($salle, $salles_invisibles)) {
+        $bloque = '<i><a href="bloque_salle.php?a=b&s='.strtolower($salle).'">bloque</a></i>';
+        $debloque = '<i><a href="bloque_salle.php?a=d&s='.strtolower($salle).'">debloque</a></i>';
+        $lien = $bloque;
+        // calcul dernière plus ancienne connexion
+        $jours_last_con = Connexion_doyenne_salle($machines_de_salle[$salle]);
+        $class_jour ='jours ';
+        if ($jours_last_con > 9)  { $class_jour = 'jours j10'; }
+        if ($jours_last_con > 19) { $class_jour = 'jours j20'; }
+        if ($jours_last_con > 29) { $class_jour = 'jours j30'; }
 
-  // lien bloque/debloque
-//if (in_array(strtolower($salle), $salles_bloquees)) { $lien = $debloque; }
- // calcul nombre de machines connectées / nombre machines de la salle
-      $nb_machines_salle = count($mdc);
-      $i = 0;
-      if (!empty($connexion_machine)) {
-          foreach($mdc as $mac) {   
-          if ( array_key_exists($mac, $connexion_machine)) { // on ne compte que les machines connectées 
-              $i++;
-          }
-      }
-  }
-      // affichage ligne de salle
-  echo "<div class=\"salle\"><a href=\"salles/?salle=$salle\">$salle</a> ($i connexions sur $nb_machines_salle machines) <span class='".$class_jour."' title='".$class_jour."'>&nbsp;&nbsp;&nbsp;&nbsp;</span> ($lien)</div>\n";
-  echo "<div class=\"connexions\"><table>\n";
+        // lien bloque/debloque
+        //if (in_array(strtolower($salle), $salles_bloquees)) { $lien = $debloque; }
+        // calcul nombre de machines connectées / nombre machines de la salle
+        $nb_machines_salle = count($mdc);
+        $i = 0;
+        if (!empty($connexion_machine)) {
+            foreach($mdc as $mac) {   
+                if ( array_key_exists($mac, $connexion_machine)) { // on ne compte que les machines connectées 
+                    $i++;
+                }
+            }
+        }
+        // affichage ligne de salle
+        echo "<a class=\"anchor\" id=\"$salle\"><div class=\"salle\"><a href=\"salles/?salle=$salle\">$salle</a> ($i connexions sur $nb_machines_salle machines) <span class='".$class_jour."' title='".$class_jour."'>&nbsp;&nbsp;&nbsp;&nbsp;</span> ($lien)</div>\n";
+        echo "<div class=\"connexions\"><table>\n";
 
-  if (!empty($connexion_machine)) {
-      // parcours des machines connectées de la salle courante
-      foreach($mdc as $mac) {
-          if (array_key_exists($mac, $connexion_machine)) { // on n'affiche que les machines connectées
-          $username = $connexion_machine[$mac]["username"];
-              $cpt = Compte($username);                       // récupère les informations sur l'utilisateur courant
-              $style = "";
-              $fin_style = "";
-              if ($cpt[2]=="Enseignant") { $style = "<b>"; $fin_style="</b>"; }
-              echo "<tr id=\"".str_replace('.','-',$connexion_machine[$mac]["ip"])."\">";
-              echo "<td><a href=\"taches.php?machine=".$mac."\">".$style.$mac.$fin_style."</a></td>";
-              echo "<td>".$style.date("H:i:s",$connexion_machine[$mac]["stamp"]).$fin_style."</td>";
-              echo "<td>".$style.$connexion_machine[$mac]["ip"].$fin_style."</td>";
-              echo "<td>".$style.$username.$fin_style."</td>"; 
-              echo "<td>".$style.$cpt[1]." ".$cpt[0].$fin_style."</td>";
-              echo "<td>".$style.$cpt[2].$fin_style."</td>";
-              echo "</tr>\n";
-          }
-      }
-  }
-  echo "</table></div>\n";
-} // fin du test salle invisible
-next($machines_de_salle);
+        if (!empty($connexion_machine)) {
+        // parcours des machines connectées de la salle courante
+            foreach($mdc as $mac) { 
+                if (array_key_exists($mac, $connexion_machine)) {   // on n'affiche que les machines connectées
+                    $username = $connexion_machine[$mac]["username"];
+                    $cpt = Compte($username);                       // récupère les informations sur l'utilisateur courant
+                    $style = "";
+                    $fin_style = "";
+                    if ($cpt[2]=="Enseignant") { $style = "<b>"; $fin_style="</b>"; }
+                    echo "<tr id=\"".str_replace('.','-',$connexion_machine[$mac]["ip"])."\">";
+                    echo "<td><a href=\"taches.php?machine=".$mac."\">".$style.$mac.$fin_style."</a></td>";
+                    echo "<td>".$style.date("H:i:s",$connexion_machine[$mac]["stamp"]).$fin_style."</td>";
+                    echo "<td>".$style.$connexion_machine[$mac]["ip"].$fin_style."</td>";
+                    echo "<td>".$style.$username.$fin_style."</td>"; 
+                    echo "<td>".$style.$cpt[1]." ".$cpt[0].$fin_style."</td>";
+                    echo "<td>".$style.$cpt[2].$fin_style."</td>";
+                    echo "</tr>\n";
+                }
+            }
+        }
+        echo "</table></div><a>\n";
+    } // fin du test salle invisible
+    next($machines_de_salle);
 }
+
 // connexions wifi
 /*
 $connexions_wifi = Connexions_wifi();
