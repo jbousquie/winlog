09ac20f39d1d7091850aa2592ac115064ee58c05
jbousquie <jerome@bousquie.fr>
2016-11-21 10:36:47 +0100

    ajout exclusions OU

---
+++ admin/custom.php
@@ -0,0 +1,13 @@
+<?php
+// Ce fichier contient les fonctions de customisation de Winlog pour des besoins spécifiques à chaque établissement.
+// Exemple : affecter le nom d'une salle à une machine à partir de son nom et pas de son OU de rangement dans AD
+
+// Retourne un nom de salle à partir d'un nom de machine ou retourne un nom par défaut
+function SalleDeMachine($machine, $defaut) {
+    $salle = $defaut;
+
+
+    return $salle;
+};
+
+?>
