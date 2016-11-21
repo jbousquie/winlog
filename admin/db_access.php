0dab9dd247e18a0b5dffa96197f1df49d6ab0db9
jbousquie <jerome@bousquie.fr>
2016-11-21 14:35:20 +0100

    fix encodage utf8 client mysqli

--- admin/db_access.php
+++ admin/db_access.php
@@ -4,33 +4,34 @@
 function db_connect() {
     global $db_server, $db_user, $db_passwd, $db_dbname;
     $db = new mysqli($db_server, $db_user, $db_passwd, $db_dbname);
+    $db->set_charset('utf8');
     return $db;
 }
 
 // Fonction : db_query
 // Renvoie le résultat d'une requête $query sur la base $db
-function db_query($db, $query) {
+function db_query(&$db, $query) {
     $res = $db->query($query);
     return $res;
 }
 
 // Fonction : db_fetch_row
 // Renvoie un tableau indexé à partir d'une ligne de résultat d'une requête
-function db_fetch_row($res) {
+function db_fetch_row(&$res) {
     $row = $res->fetch_row();
     return $row;
 }
 
 // Fonction db_escape_string
 // Retourne ûne chaîne échappée
-function db_escape_string($db, $string) {
+function db_escape_string(&$db, $string) {
     $str = $db->real_escape_string($string);
     return $str;
 }
 
 // Fonction : db_free
 // Libère la mémoire
-function db_free($res) {
+function db_free(&$res) {
     $res->free();
 }
 ?>
