c94d5aa86b374b365074c51300798b2936a160f4
jbousquie <jerome@bousquie.fr>
2016-11-24 14:09:43 +0100

    cosmétique

--- admin/client_http.php
+++ admin/client_http.php
@@ -1,9 +1,10 @@
 <?php
 // Client HTTP
+// Ce fichier contient les fonctions clientes http
 require_once 'HTTP/Request2.php';
 
 // Fonction GetURL() : renvoie le contenu d'une réponse à un GET http
-// renvoie une string contenant le corps de la réponse
+// renvoie une string contenant le corps de la réponse http
 Function GetURL($url) {
     $body = "";
     $r = new HTTP_Request2($url, HTTP_Request2::METHOD_GET);
@@ -21,6 +22,7 @@ Function GetURL($url) {
 
 // Fonction PostURL() : émet un POST http sur l'url
 // paramètre : string $url et $param, un array de paramètres du POST
+// retourne une string contenant le corps de la réponse http
 Function PostURL($url, $params) {
     $body = "";
     $http = new HTTP_Request2( $url, HTTP_Request2::METHOD_POST);
