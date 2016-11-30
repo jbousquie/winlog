55280146b1b7e7312474fba3db1c17ccaade91fa
jbousquie <jerome@bousquie.fr>
2016-11-30 10:39:20 +0100

    ajout lien pour enseignants vers ma_salle_live.php

--- admin/interdit.php
+++ admin/interdit.php
@@ -1,6 +1,9 @@
 <?php
-include_once('libhome.php');
-$username = phpCAS::getUser();
+include_once('session.php');
+$username = Username();
+$profil = Profil($username);
+$role = $roles[$profil];
+
 ?>
 <!DOCTYPE HTML>
 <html lang="fr">
@@ -11,7 +14,14 @@ $username = phpCAS::getUser();
 </head>
 <body>
     <p class="header">WINLOG</p>
-    <p>compte : <i><?php echo($username); ?></i></p>
-    <p>Vous n'avez pas les autorisations requises pour utiliser Winlog</p>
+    <p>compte : <i><?php echo($username); ?></i><br/>rôle : <i><?php echo($role); ?></i></p>
+    <p>Vous n'avez pas les autorisations requises pour utiliser la console d'administration de Winlog</p>
+    <?php 
+        if ($profil >= $niveaux[$roles[1]]) {
+    ?>
+    <p>Peut-être souhaitez-vous simplement voir les <a href="ma_salle_live.php">connexions en cours de votre salle de cours</a> ?</p>
+    <?php
+        }
+    ?>
 </body>
 </html>
