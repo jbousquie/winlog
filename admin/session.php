820416dbde2a6b683c6cb74105defc83c1736986
jbousquie <jerome@bousquie.fr>
2016-11-30 09:34:19 +0100

    fix Profil()

--- admin/session.php
+++ admin/session.php
@@ -1,11 +1,16 @@
 <?php
 // Ce fichier contient la gestion de la gestion applicative Winlog, différente de la session CAS/
-// Il démarre une session php. Ne pas appeler en même temps que libhome.php qui démarre une session php pour CAS.
 include_once('winlog_admin_conf.php');
-session_start();
 
-if (!isset($_SESSION['username'])) {
-    header('Location: '.$winlog_url);
+// fonction Username() vérifie que la session PHP existe et retourne $username, sinon renvoie sur $winlog_url
+function Username() {
+    session_start();
+    global $_SESSION, $winlog_url;
+    if (!isset($_SESSION['username'])) {
+        header('Location: '.$winlog_url);
+        exit();
+    }
+    return $_SESSION['username'];
 }
 
 // Fonction Profil() : renvoie la valeur du role de $username
@@ -14,14 +19,33 @@ if (!isset($_SESSION['username'])) {
 // autre            => 0
 function Profil($username) {
     global $administrateurs, $superviseurs;
+    global $roles, $niveaux, $lib_personnel;
     if (in_array($username, $administrateurs)) {
-        return 2;
+        return $niveaux[$roles[3]];
     } 
     elseif (in_array($username, $superviseurs)) {
-        return 1;
+        return $niveaux[$roles[2]];
     }
     else {
-        return 0;
+        $compte = Compte($username);
+        if ($compte[2] == $lib_personnel) {
+            return $niveaux[$roles[1]];
+        }
+        else
+        {
+            return $niveaux[$roles[0]];
+        }
     }
-}
+};
+
+// Fonction FiltreProfil() : redirige vers interdit.php si profil pas autorisé
+function FiltreProfil($profil) {
+    if ($profil == 0) {
+        header('Location: interdit.php');
+        exit();
+    }
+
+};
+
+
 ?>
