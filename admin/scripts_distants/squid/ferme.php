cf8e2c044797020f645b7b116e3b980fa26a2bef
jbousquie <jerome@bousquie.fr>
2016-11-30 11:46:00 +0100

    ajout ferme.php

---
+++ admin/scripts_distants/squid/ferme.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<div><img src="http://cache.iut-rodez.fr/warnings/vr_tn_IUT_Rodez.jpg"></div>
+<div><h2>L'accès au Web depuis cette salle est momentanément fermé.</h2></div>
+</body>
+</html>
