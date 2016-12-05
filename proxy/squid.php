9dc3350dfdffca25f518dd04915781b129e5923f
jbousquie <jerome@bousquie.fr>
2016-12-05 15:56:59 +0100

    acces direct à squid.php depuis le browser

--- proxy/squid.php
+++ proxy/squid.php
@@ -7,35 +7,9 @@ $src = $_GET["src"];
 $target = $_GET["tgt"];
 $username = "";
 
-$prefixe_reseau_captif = "10.3.";             // prefixe du réseau captif
-
-if ($src!="etudiants" && $src!="personnels") {
-  // si src <> étudiants ou personnels :
-  // la requête vient directement de cache.iut-rodez.fr : $ip reçu est fiable
-  // il s'agit d'un accès interdit depuis un poste fixe de l'IUT
-  // sinon :
-  // la requête vient d'un iframe inclus dans la page interdit.php
-  // on ne peut se baser que sur X_FORWARDED_FOR
-
-  $adresses = explode(',', $_SERVER["HTTP_X_FORWARDED_FOR"]); 
-        // le header peut contenir plusieurs ip (ex: routeur Kanet) => on récupère un tableau
-  $ip = "";
-  $lg_prefixe = strlen($prefixe_reseau_captif);
-  foreach($adresses as $http_ip) {
-    if (substr($http_ip, 0, $lg_prefixe) == $prefixe_reseau_captif) { $ip = $http_ip; }
-  }
-  
-  $connexions_wifi = Connexions_wifi();
-  $i = 0;
-  while ($connexions_wifi[$i]) {
-  if ($connexions_wifi[$i]["ip"] == $ip) { $username = $connexions_wifi[$i]["username"];}
-  $i++;
-  }
-
-}
-else { // src= etudiants ou personnels
-  $con_ip = Con_ip($ip);
-  if (!empty($con_ip)) { $username = $con_ip[1]; }
+$con_ip = Con_ip($ip);
+if (!empty($con_ip)) { 
+    $username = $con_ip[1]; 
 }
 
 $db = db_connect();
