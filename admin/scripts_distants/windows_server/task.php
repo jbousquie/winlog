28cf92e4f1c66ff10543c33a8a69fa93ed28d68b
jbousquie <jerome@bousquie.fr>
2016-12-09 14:43:37 +0100

    $domaine

--- admin/scripts_distants/windows_server/task.php
+++ admin/scripts_distants/windows_server/task.php
@@ -2,11 +2,12 @@
 // Ce script renvoie au format json la liste des tâches en cours sur une machine $host
 header('Content-type: application/json; charset=utf-8');
 include('lib.php');
+$domaine = "IUT";
 $hote = $_GET['host'];
 // Récupérer sur un POST, récupérer en https
 // ajouter une clé partagée à tester
 //$command = "tasklist /fo csv /nh /v /u IUT\Administrateur /p ".Decrypte($mdp, $cle)." /s ".$hote." | findstr IUT | sort";
-$command = "tasklist /fo csv /nh /v /u IUT\Administrateur /p ".Decrypte($mdp, $cle)." /s ".$hote." | sort";
+$command = "tasklist /fo csv /nh /v /u $domaine\Administrateur /p ".Decrypte($mdp, $cle)." /s ".$hote." | sort";
 //$command = 'task.bat "'.Decrypte($mdp, $cle).'" '.$hote;
 $res = array();
 $tab_task = array();
